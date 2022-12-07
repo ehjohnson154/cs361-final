a15188083636a673d82661c1f0934d62f9e2e5ae
ehjohnson154 <spiffy154@gmail.com>
2022-12-07 12:10:09 -0800

    removed point

--- gis.rb
+++ gis.rb
@@ -56,16 +56,6 @@ class TrackSegment #only used by track
   end
 end
 
-class Point
-
-  attr_reader :lat, :lon, :ele
-
-  def initialize(lon, lat, ele=nil)
-    @lon = lon
-    @lat = lat
-    @ele = ele
-  end
-end
 
 class Waypoint
 
@@ -137,16 +127,16 @@ def main()
   w = Waypoint.new(-121.5, 45.5, 30, "home", "flag") #assigns new waypoint details
   w2 = Waypoint.new(-121.5, 45.6, nil, "store", "dot") 
   ts1 = [ 
-  Point.new(-122, 45),
-  Point.new(-122, 46),
-  Point.new(-121, 46),
+  Waypoint.new(-122, 45),
+  Waypoint.new(-122, 46),
+  Waypoint.new(-121, 46),
   ]
 
-  ts2 = [ Point.new(-121, 45), Point.new(-121, 46), ]
+  ts2 = [ Waypoint.new(-121, 45), Waypoint.new(-121, 46), ]
 
   ts3 = [
-    Point.new(-121, 45.5),
-    Point.new(-122, 45.5),
+    Waypoint.new(-121, 45.5),
+    Waypoint.new(-122, 45.5),
   ]
 
   t = Track.new([ts1, ts2], "track 1") #creates cracks out of points
